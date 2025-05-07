2ce3a64e735aa3514ecf030b35b171372ecbb4cd
Imperiumjervi <rodriguez.jefferson@correounivalle.edu.co>
2025-05-06 23:18:18 -0500

    Segundo commit

--- Ejemplo1/src/main.cpp
+++ Ejemplo1/src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <string>
 
 int main() {
-    std::cout << "Hola mundo desde el comando NewProject" << std::endl;
-    return 0;
+  std::cout << "Hola mundo desde el comando NewProject" << std::endl;
+  std::cout << "Este es un nuevo proyecto de C++" << std::endl;
+  return 0;
 }
