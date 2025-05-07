8f1e900c74aabf62346c916de8c664ee079a6ff2
Imperiumjervi <rodriguez.jefferson@correounivalle.edu.co>
2025-05-07 11:25:22 -0500

    Install con copia de seguridad

--- install.sh
+++ install.sh
@@ -5,6 +5,12 @@ echo "Instalando configuracion de neovim"
 #Destino
 NVIM_CONFIG=$HOOME/.config/nvim 
 
+if [ -d "$NVIM_CONFIG"]; then
+  BACKUP_DIR="$HOME/.config/nvim-backup-$(date +%Y%m%d%H%M%S)"
+  echo "El directorio de configuracion de neovim ya existe. Haciendo copia de seguridad en $BACKUP_DIR"
+  mv "$NVIM_CONFIG" "$BACKUP_DIR"
+fi
+
 mkdir -p "$NVIM_CONFIG"
 
 cp -r ./nvim/* $NVIM_CONFIG
